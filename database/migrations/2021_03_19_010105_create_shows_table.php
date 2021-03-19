f1374bd12ea94f69b75c8a74db7b03d0b224e4b9
Jaydel Bujawe <jidbujawe@gmail.com>
2021-03-19 13:37:44 +1100

    Adding database
    Created migrations, models, factories for entities:
    
    Show
    Celeb
    Genre
    
    Pivot tables:
    genre_movie
    genre_show
    celeb_show

---
+++ database/migrations/2021_03_19_010105_create_shows_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateShowsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('shows', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->mediumText('synopsis');
+            $table->year('year');
+            $table->string('poster');
+            $table->string('trailer');
+            $table->integer('seasons');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('shows');
+    }
+}
