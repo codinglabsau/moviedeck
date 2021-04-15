8b6d73c3571ec01bf96a812e2536b149e3cd8823
KieranLacey <kieran.lacey79@gmail.com>
2021-04-15 11:34:41 +1000

    Added test suites for registration and login

--- tests/Feature/Http/Controllers/Auth/RegisterControllerTest.php
+++ tests/Feature/Http/Controllers/Auth/RegisterControllerTest.php
@@ -8,15 +8,39 @@ use Tests\TestCase;
 
 class RegisterControllerTest extends TestCase
 {
+    use RefreshDatabase;
+
     /** @test */
     public function user_is_created_when_they_register()
     {
-        $response = $this->getJson('/register');
+        $this->withoutExceptionHandling();
 
-        $response->assertJsonFragment([
+        $response = $this->post('/register', [
             'name' => 'Joseph Hand',
             'email' => 'joseph.hand@example.com',
-            'password' => 'secret'
+            'password' => 'secretPass',
+            'password_confirmation' => 'secretPass'
         ]);
+
+        $response->assertRedirect(route('home'));
+
+        $this->assertDatabaseHas('users', [
+            'name' => 'Joseph Hand',
+            'email' => 'joseph.hand@example.com'
+        ]);
+    }
+
+    /** @test */
+    public function only_an_authenticated_user_can_see_home_view()
+    {
+
+        $this->getJson('home')
+             ->assertStatus(401);
+
+        $user = \App\Models\User::factory()->create();
+
+        $this->actingAs($user)
+             ->getJson('home')
+             ->assertOk();
     }
 }
