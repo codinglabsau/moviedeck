f69e462fedcd59013be347f829df92aec84e8cde
vrodriguero <vickyamauti@hotmail.com>
2021-04-30 16:25:14 +1000

    changed structure for the tests directory

---
+++ tests/Http/Middleware/AdminMiddlewareTest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Tests\Http\Middleware;
+
+use Tests\TestCase;
+use App\Models\User;
+use Illuminate\Support\Facades\Route;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class AdminMiddlewareTest extends TestCase
+{
+    use RefreshDatabase;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        Route::get('/admin-route', function () {
+            return 'nice';
+        })->middleware(['web', 'admin']);
+    }
+
+    /** @test */
+    public function non_admin_are_redirected()
+    {
+        $user = User::factory()->create();
+
+        $this->actingAs($user)
+            ->getJson('/admin-route')
+            ->assertRedirect();
+    }
+
+    /** @test */
+    public function admin_are_redirected()
+    {
+        $user = User::factory()->admin()->create();
+
+        $this->actingAs($user)
+            ->getJson(route('celebs.create'))
+            ->assertOk();
+    }
+}
