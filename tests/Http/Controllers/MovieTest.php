4659052a619b867ea296b3241e76c90dde22a84b
vrodriguero <vickyamauti@hotmail.com>
2021-04-30 16:13:47 +1000

    wip: - Moved tests from middleware over to controller

--- tests/Http/Controllers/MovieTest.php
+++ tests/Http/Controllers/MovieTest.php
@@ -12,6 +12,102 @@ class MovieTest extends TestCase
     use RefreshDatabase;
 
     /** @test */
+    public function an_admin_can_access_movies_view()
+    {
+        $admin = User::factory()->admin()->create();
+
+        $this->actingAs($admin)
+            ->get('/movies')
+            ->assertOk();
+    }
+
+    /** @test */
+    public function a_user_can_access_movies_view()
+    {
+        $user = User::factory()->create([
+            'is_admin' => false,
+        ]);
+
+        $this->actingAs($user)
+            ->get('/movies')
+            ->assertOk();
+    }
+
+    /** @test */
+    public function a_guest_can_access_movies_view()
+    {
+        $user = User::factory()->create([
+            'is_admin' => false,
+        ]);
+
+        $this->actingAs($user)
+            ->get('/movies')
+            ->assertOk();
+    }
+
+    /** @test */
+    public function an_admin_can_access_create_movie_view()
+    {
+        $admin = User::factory()->admin()->create();
+
+        $this->actingAs($admin)
+            ->get('/movies/create')
+            ->assertOk();
+    }
+
+    /** @test */
+    public function a_user_cannot_access_create_movie_view()
+    {
+        $user = User::factory()->create([
+            'is_admin' => false
+        ]);
+
+        $this->actingAs($user)
+            ->get('/movies/create')
+            ->assertRedirect();
+    }
+
+    /** @test */
+    public function a_guest_cannot_access_create_movie_view()
+    {
+        $this->get('/movies/create')
+            ->assertRedirect();
+    }
+
+    /** @test */
+    public function an_admin_can_access_edit_movie_view()
+    {
+        $movie = Movie::factory()->create();
+        $admin = User::factory()->admin()->create();
+
+        $this->actingAs($admin)
+            ->get("/movies/{$movie->id}/edit")
+            ->assertOk();
+    }
+
+    /** @test */
+    public function a_user_cannot_access_edit_movie_view()
+    {
+        $movie = Movie::factory()->create();
+        $user = User::factory()->create([
+            'is_admin' => false
+        ]);
+
+        $this->actingAs($user)
+            ->get("/movies/{$movie->id}/edit")
+            ->assertRedirect();
+    }
+
+    /** @test */
+    public function a_guest_cannot_access_edit_movie_view()
+    {
+        $movie = Movie::factory()->create();
+
+        $this->get("/movies/{$movie->id}/edit")
+            ->assertRedirect();
+    }
+
+    /** @test */
     public function an_admin_can_add_a_movie()
     {
         $admin = User::factory()->admin()->create();
--- tests/Http/Controllers/ReviewTest.php
+++ tests/Http/Controllers/ReviewTest.php
@@ -13,6 +13,103 @@ class ReviewTest extends TestCase
     use RefreshDatabase;
 
     /** @test */
+    public function an_auth_user_can_access_reviews_view()
+    {
+        $user = User::factory()->create();
+
+        $this->actingAs($user)
+            ->getJson('/reviews')
+            ->assertOk();
+    }
+
+    /** @test */
+    public function a_guest_can_access_reviews_view()
+    {
+        $this->getJson('/reviews')
+            ->assertOk();
+    }
+
+    /** @test */
+    public function any_auth_user_can_access_create_reviews_view()
+    {
+        $movie = Movie::factory()->create();
+        $user = User::factory()->create();
+
+        $this->actingAs($user)
+            ->getJson("/reviews/create/$movie->id")
+            ->assertOk();
+    }
+
+    /** @test */
+    public function a_guest_cannot_access_create_reviews_view()
+    {
+        $movie = Movie::factory()->create();
+
+        $this->getJson("/reviews/create/$movie->id")
+            ->assertUnauthorized();
+    }
+
+    /** @test */
+    public function a_user_can_access_edit_view_of_their_review()
+    {
+        $user = User::factory()->create();
+        $movie = Movie::factory()->create();
+        $review = Review::factory()->create([
+            'user_id' => $user->id,
+            'movie_id' => $movie->id,
+            'title' => 'Review Title Sample',
+            'rating' => 4.8,
+            'content' => 'Sample review content',
+        ]);
+
+        $this->actingAs($user)
+            ->get("/reviews/{$review->id}/edit")
+            ->assertOk();
+    }
+
+    /** @test */
+    public function a_user_cannot_access_edit_view_of_a_review_they_did_not_create()
+    {
+        $user = User::factory()->create([
+            'id' => 1
+        ]);
+        $movie = Movie::factory()->create();
+        $review = Review::factory()->create([
+            'user_id' => $user->id,
+            'movie_id' => $movie->id,
+            'title' => 'Review Title Sample',
+            'rating' => 4.8,
+            'content' => 'Sample review content',
+        ]);
+
+        $anotherUser = User::factory()->create([
+            'id' => 2
+        ]);
+
+        $this->actingAs($anotherUser)
+            ->get("/reviews/{$review->id}/edit")
+            ->assertRedirect()
+            ->assertSessionHas('status');
+    }
+
+    /** @test */
+    public function a_guest_cannot_access_edit_review_view()
+    {
+        $user = User::factory()->create();
+        $movie = Movie::factory()->create();
+        $review = Review::factory()->create([
+            'user_id' => $user->id,
+            'movie_id' => $movie->id,
+            'title' => 'Review Title Sample',
+            'rating' => 4.8,
+            'content' => 'Sample review content',
+        ]);
+
+        $this->get("/reviews/{$review->id}/edit")
+            ->assertRedirect();
+    }
+
+    /** @test */
     public function any_auth_user_can_add_a_review()
     {
         $movie = Movie::factory()->create();
