155c331b43006b9fa8847d431817e2a33240a098
KieranLacey <kieran.lacey79@gmail.com>
2021-05-06 16:47:11 +1000

    Adding in the reviews on the profile.dashboard

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -4,11 +4,19 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\User;
+use App\Models\Review;
 
 class ProfileController extends Controller
 {
     public function dashboard(User $user)
     {
-        return view('profile/dashboard')->with('user', $user);
+        $reviews = Review::orderBy('created_at')
+            ->with('movies', 'users')
+            ->take(4);
+
+        return view('profile/dashboard', [
+            'user' => $user,
+            'reviews' => $reviews
+        ]);
     }
 }
