d30522fe03d5db33af0fb1b08e14400a4f2d5240
KieranLacey <kieran.lacey79@gmail.com>
2021-05-21 17:05:15 +1000

    Followed suggested changes on PR until testing suggestions

--- resources/views/profile/dashboard.blade.php
+++ resources/views/profile/dashboard.blade.php
@@ -47,7 +47,7 @@
 
                         <span class="mx-4 font-medium">Reviews</span>
                     </a>
-                    @if (Auth::user()==$user)
+                    @if (auth()->user()==$user)
                         <a class="flex items-center px-4 py-2 mt-5 text-gray-100 transition-colors duration-200 transform hover:bg-gray-700 hover:text-gray-200" href="{{ route('watchlist.index', $user->id) }}">
                             <svg class="w-5 h-5" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                                 <path d="M15 5V7M15 11V13M15 17V19M5 5C3.89543 5 3 5.89543 3 7V10C4.10457 10 5 10.8954 5 12C5 13.1046 4.10457 14 3 14V17C3 18.1046 3.89543 19 5 19H19C20.1046 19 21 18.1046 21 17V14C19.8954 14 19 13.1046 19 12C19 10.8954 19.8954 10 21 10V7C21 5.89543 20.1046 5 19 5H5Z" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
@@ -69,9 +69,9 @@
             <div class="container w-full px-14 py-5 mx-auto">
                 <div class="flex justify-between">
                     <span class="font-medium text-gray-800 whitespace-nowrap capitalize md:text-2xl">Recent Reviews</span>
-                    @if (Auth::user()==$user)
+                    @if (auth()->user()==$user)
                         <div class="flex items-center px-4 py-2 font-medium tracking-wide capitalize transition-colors duration-200 transform rounded-md border-2 border-gray-300">
-                            <a href="{{ route('profile.reviews', $user->id) }}"><span class="mx-2 whitespace-nowrap">{{$review_count}} | Manage Reviews</span></a>
+                            <a href="{{ route('profile.reviews', $user->id) }}"><span class="mx-2 whitespace-nowrap">{{$user->reviews_count}} | Manage Reviews</span></a>
                         </div>
                     @endif
                 </div>
@@ -80,7 +80,7 @@
                         @foreach($reviews as $review)
                             <div class="w-full max-w-xs flex justify-start text-center">
                                 <div class="flex flex-col">
-                                    <img class="object-cover object-center flex h-16 md:h-32 mx-auto justify-start" src={{$review->movie->poster}} alt="movie_poster"/>
+                                    <img class="object-cover object-center flex h-16 md:h-32 mx-auto justify-start" src="{{$review->movie->poster}}" alt="movie_poster"/>
                                     <div class="text-lg flex font-medium leading-none text-gray-700 ">{{$review->movie->title}}</div>
                                 </div>
                                 <div class="mt-1 flex flex-col items-start">
@@ -120,7 +120,7 @@
                     <div class="flex justify-between">
                         <span class="font-medium text-gray-800 whitespace-nowrap capitalize md:text-2xl">Watch Next</span>
                         <div class="flex items-center px-4 py-2 font-medium tracking-wide capitalize transition-colors duration-200 transform rounded-md border-2 border-gray-300">
-                            <a href="{{ route('watchlist.index', $user->id) }}"><span class="mx-2 whitespace-nowrap">{{$watchlist_count}} | Manage List</span></a>
+                            <a href="{{ route('watchlist.index', $user->id) }}"><span class="mx-2 whitespace-nowrap">{{$user->watchlist_count}} | Manage List</span></a>
                         </div>
                     </div>
                     <div class="flex items-baseline justify-center">
