c76e2743b160b470b4a96a351f8374ff48f74d10
Jaydel Bujawe <jidbujawe@gmail.com>
2021-05-24 10:56:02 +1000

    Added star with rating in movies index

--- resources/views/movies/index.blade.php
+++ resources/views/movies/index.blade.php
@@ -32,7 +32,12 @@
                                 </a>
                                 <div class="flex justify-between my-6">
                                     <h3>{{ $movie->title }}</h3>
-                                    <h3>{{ $movie->average_rating }}</h3>
+                                    <h3 class="flex flex-row">
+                                        <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 align-middle mr-1 mt-1" viewBox="0 0 20 20" fill="currentColor">
+                                            <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
+                                        </svg>
+                                        {{ $movie->average_rating }}
+                                    </h3>
                                 </div>
                             </div>
                             <div class="flex flex-row">
