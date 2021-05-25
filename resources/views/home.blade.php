86f6940845b0774de3bd7ad1ef75a708c2724e08
Jaydel Bujawe <jidbujawe@gmail.com>
2021-05-25 13:22:16 +1000

    Update home.blade.php
    - Added star icon with rating
    - Added link to review creator

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -82,7 +82,12 @@
                             </a>
                             <div class="flex justify-between">
                                 <h3>{{ $movie->title }}</h3>
-                                <h3>{{ $movie->average_rating }}</h3>
+                                <h3 class="flex flex-row">
+                                    <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 align-middle mr-1 mt-1" viewBox="0 0 20 20" fill="currentColor">
+                                        <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
+                                    </svg>
+                                    {{ $movie->average_rating }}
+                                </h3>
                             </div>
                         </div>
                     @endforeach
@@ -149,7 +154,9 @@
                                     <span class="font-normal text-sm">/ 10</span> </h1>
                                 <h2 class="font-medium pt-2 text-md"> {{ $review->title }} </h2>
                             </a>
-                            <span class="font-bold text-sm text-blue-500 mr-4"> {{ $review->user->name }} </span><br/>
+                            <a href="{{ route('profile.dashboard', $review->user->id) }}">
+                                <span class="font-bold text-sm text-blue-500 mr-4"> {{ $review->user->name }} </span>
+                            </a><br/>
                             <span class="font-normal text-sm"> {{ $review->created_at->diffForHumans() }} </span>
                             <p class="mt-6">
                                 {{ $review->excerpt }}
