1b8aff98128496f943fcafde98b470894a885dbf
Jaydel Bujawe <jidbujawe@gmail.com>
2021-05-27 11:46:13 +1000

    Added small text style for buttons to align in reviews edit and create

--- resources/views/reviews/create.blade.php
+++ resources/views/reviews/create.blade.php
@@ -67,7 +67,7 @@
                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M14.752 11.168l-3.197-2.132A1 1 0 0010 9.87v4.263a1 1 0 001.555.832l3.197-2.132a1 1 0 000-1.664z" />
                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
                             </svg>
-                            <span class="mx-2 whitespace-nowrap">Play Trailer</span>
+                            <span class="mx-2 whitespace-nowrap text-sm">Play Trailer</span>
                         </button>
                         @if(auth()->check())
                             <form method="post" action="{{ route('watchlist.store', ['user'=>auth()->user()->id, 'movie'=>$movie->id]) }}">
@@ -76,7 +76,7 @@
                                     <svg class="w-5 h-5 mx-1" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="#f8f8f8">
                                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 5a2 2 0 012-2h10a2 2 0 012 2v16l-7-3.5L5 21V5z" />
                                     </svg>
-                                    <span class="text-gray-300 mx-2 whitespace-nowrap font-normal">Add to Watchlist</span>
+                                    <span class="text-gray-300 mx-2 whitespace-nowrap font-normal text-sm">Add to Watchlist</span>
                                 </button>
                             </form>
                         @endif
